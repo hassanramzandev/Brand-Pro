d83e259641d3c4f56afe664a1725c1bcea3ffeb2
hassanramzandev <hassanramzan319040@gmail.com>
2026-07-17 15:05:45 -0700

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+ window.addEventListener('scroll',function(){
+              
+      var navmenu=document.getElementById('navMenu');
+           if(navmenu.classList.contains('show')){
+            if(navmenu.classList.remove('show'));
+           }
+    });
